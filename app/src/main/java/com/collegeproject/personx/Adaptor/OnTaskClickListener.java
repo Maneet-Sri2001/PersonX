2cf52183462b82e4c6258d33ff4eef687c595f0c
unknown <sshivamppandey3022@gmail.com>
2022-05-08 20:08:58 +0530

    all task are categorized

--- app/src/main/java/com/collegeproject/personx/Adaptor/OnTaskClickListener.java
+++ app/src/main/java/com/collegeproject/personx/Adaptor/OnTaskClickListener.java
@@ -1,7 +1,10 @@
 package com.collegeproject.personx.Adaptor;
 
+
+
 import com.collegeproject.personx.Model.TaskModel;
 
 public interface OnTaskClickListener {
   void onTaskClick(TaskModel taskModel);
+
 }
