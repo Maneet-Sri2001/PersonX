d1f4da4ff3dd8d2b7cc47b1d29d9c4f9a3cecfdc
Maneet Srivastav <maneet769@gmail.com>
2022-05-08 20:30:08 +0530

    Unknown

--- app/src/main/java/com/collegeproject/personx/OnBoardActivity.java
+++ app/src/main/java/com/collegeproject/personx/OnBoardActivity.java
@@ -1,9 +1,5 @@
 package com.collegeproject.personx;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
@@ -11,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import com.collegeproject.personx.Adaptor.ViewPagerAdaptor;
 import com.collegeproject.personx.Utils.SharedPreferenceClass;
@@ -23,6 +23,7 @@ public class OnBoardActivity extends AppCompatActivity {
   ViewPagerAdaptor viewPagerAdaptor;
   
   private SharedPreferenceClass sharedPreferenceClass;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -30,15 +31,15 @@ public class OnBoardActivity extends AppCompatActivity {
     backBtn = findViewById(R.id.backBtn);
     nextBtn = findViewById(R.id.nextBtn);
     skipBtn = findViewById(R.id.skipBtn);
-  
+    
     sharedPreferenceClass = new SharedPreferenceClass(getApplicationContext());
-  
+    
     backBtn.setOnClickListener(view -> {
       if (getItem(0) > 0) {
         mSlideViewPager.setCurrentItem(getItem(-1), true);
       }
     });
-  
+    
     nextBtn.setOnClickListener(view -> {
       backBtn.setVisibility('1');
       if (getItem(0) < 3) {
@@ -50,22 +51,22 @@ public class OnBoardActivity extends AppCompatActivity {
         finish();
       }
     });
-  
+    
     skipBtn.setOnClickListener(view -> {
       Intent intent = new Intent(OnBoardActivity.this, RegisterActivity.class);
       startActivity(intent);
       sharedPreferenceClass.setValueBoolean("userBoarded", true);
       finish();
-    
+      
     });
-  
+    
     mSlideViewPager = findViewById(R.id.slideViewPager);
     mDotLayout = findViewById(R.id.indicator_layout);
-  
+    
     viewPagerAdaptor = new ViewPagerAdaptor(this);
-  
+    
     mSlideViewPager.setAdapter(viewPagerAdaptor);
-  
+    
     setUpIndicator(0);
     mSlideViewPager.addOnPageChangeListener(viewListener);
   }
@@ -95,9 +96,9 @@ public class OnBoardActivity extends AppCompatActivity {
       setUpIndicator(position);
       if (position > 0) {
         backBtn.setVisibility(View.VISIBLE);
-      } else {
+      } else
         backBtn.setVisibility(View.INVISIBLE);
-      }
+      
     }
     
     @Override
